b5029ca0c326bceca6533dd3da72598991972f8e
zecosta.monteiro <zecosta.monteiro@gmail.com>
2019-11-07 11:59:25 -0300

    add data section and malawi fertility data 2008

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Measures of fertility from the Malawi 2008 Census
+#'
+#' Measures of fertility from the Malawi 2008 Census
+#'
+#' @format
+#'   A data frame with 3 variables:
+#'   ages (starting age of five-year age group),
+#'   P (average parity of women) and
+#'   asfr (age-specific period fertility rates)
+#'
+#' @source
+#'   Moultrie TA, RE Dorrington, AG Hill, K Hill, IM Timæus and B Zaba (eds). 2013.
+#'   Tools for Demographic Estimation. Paris: International Union for the Scientific Study of
+#'   Population. demographicestimation.iussp.org
+"mwi_2008"
