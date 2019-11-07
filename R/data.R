1e144e035e9960338d095c28f26fffe782d192a4
zecosta.monteiro <zecosta.monteiro@gmail.com>
2019-11-07 13:03:53 -0300

    change data names and add parity data

--- R/data.R
+++ R/data.R
@@ -12,4 +12,34 @@
 #'   Moultrie TA, RE Dorrington, AG Hill, K Hill, IM Timæus and B Zaba (eds). 2013.
 #'   Tools for Demographic Estimation. Paris: International Union for the Scientific Study of
 #'   Population. demographicestimation.iussp.org
-"mwi_2008"
+"mwi_fert_2008"
+
+#' Parity data from Cambodia 2008 Census
+#'
+#' Parity data from Cambodia 2008 Census
+#'
+#' @format
+#'   A data frame with 9 variables:
+#'   parity (from 0 to 30 and including NA responses),
+#'   PX (number of women reffered to specific parity of age group X, X from 0:10-14 to 7:45-49)
+#'
+#' @source
+#'   Moultrie TA, RE Dorrington, AG Hill, K Hill, IM Timæus and B Zaba (eds). 2013.
+#'   Tools for Demographic Estimation. Paris: International Union for the Scientific Study of
+#'   Population. demographicestimation.iussp.org
+"khm_par_2008"
+
+#' Parity data from Kenya 1989 Census
+#'
+#' Parity data from Kenya 1989 Census
+#'
+#' @format
+#'   A data frame with 8 variables:
+#'   parity (from 0 to 30 and including NA responses),
+#'   PX (number of women reffered to specific parity of age group X, X from 1:15-19 to 7:45-49)
+#'
+#' @source
+#'   Moultrie TA, RE Dorrington, AG Hill, K Hill, IM Timæus and B Zaba (eds). 2013.
+#'   Tools for Demographic Estimation. Paris: International Union for the Scientific Study of
+#'   Population. demographicestimation.iussp.org
+"ken_par_1989"
