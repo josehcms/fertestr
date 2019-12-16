e4ec039c4a740035db4ddbdfe6d0ec31cbb23e00
zecosta.monteiro <zecosta.monteiro@gmail.com>
2019-12-16 19:18:45 -0300

    update data with Brass PF mulitpliers

--- R/data.R
+++ R/data.R
@@ -45,3 +45,49 @@
 #'   Tools for Demographic Estimation. Paris: International Union for the Scientific Study of
 #'   Population. demographicestimation.iussp.org
 "data.prty_KEN"
+
+#' Original Brass PF multipliers for linear interpolation
+#'
+#' Original Brass PF multipliers for linear interpolation
+#'
+#' @format
+#'   A data frame with age coefficients for linear interpolation of cumulate fertility schedules based
+#'   on mean age at childbearing (m), parity ratio (P1/P2) and age-specific fertility rates
+#'   ratio (f1/f2).
+#'
+#' @source
+#' Brass W. 1975. Methods for Estimating Fertility and Mortality from Limited and Defected Data.
+#' North Carolina: Carolina Population Center.
+"mult.pf_brass"
+
+#' Coale and Trussel variant for Brass PF multipliers for fertility rates computed
+#' from births in a 12-month period by age of mother at end of period
+#'
+#' Coale and Trussel variant for Brass PF multipliers for fertility rates computed
+#' from births in a 12-month period by age of mother at end of period
+#'
+#' @format
+#'   A data frame with age coefficients (a, b and c) for linear interpolation of cumulate fertility
+#'   schedules
+#'
+#' @source
+#' United Nations. 1983. Manual X: Indirect techniques for demographic estimation
+#' (United Nations publication, Sales No. E.83.XIII.2).
+"mult.cltrs_shift"
+
+
+#' Coale and Trussel variant for Brass PF multipliers for fertility rates computed
+#' from births by age of mother at delivery
+#'
+#' Coale and Trussel variant for Brass PF multipliers for fertility rates computed
+#' from births by age of mother at delivery
+#'
+#' @format
+#'   A data frame with age coefficients (a, b and c) for linear interpolation of cumulate fertility
+#'   schedules
+#'
+#' @source
+#' United Nations. 1983. Manual X: Indirect techniques for demographic estimation
+#' (United Nations publication, Sales No. E.83.XIII.2).
+"mult.cltrs_noshift"
+
