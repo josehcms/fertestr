df7fd07dc41cf939a5a4e5de838f0fafa6d3443d
josehcms <zecosta.monteiro@gmail.com>
2020-07-20 15:50:25 -0300

    update CEB function

--- R/ceb.R
+++ R/ceb.R
@@ -10,6 +10,7 @@
 #' @param ceb total children ever born by women age group
 #' @param mac mean age at childbearing matching ages in age_w or unique value for all ages,
 #' default = 28
+#' @param location_mac retrieve MAC from a given location id or name from WPP2019
 #' @param plot_ceb dummy variable to return the plot for mean CEB results
 #'
 #' @return data.frame with 2 vectors: reference date of mean children ever born (decimal)
@@ -33,9 +34,34 @@ ceb_eval <-
             pop_w,
             ceb,
             mac = 28,
+            location_mac = NULL,
             plot_ceb = TRUE ){
 
     year_svy <- decimal_anydate( date_svy )
+
+
+    if( !is.null( location_mac ) ){
+
+      if ( !is.numeric( location_mac ) ){
+
+        location_code <- get_location_code( location_mac )
+
+      } else {
+
+        location_code <- location_mac
+
+      }
+
+      mac <- fetch_mac_Wpp2019( country_code = location_code, year = year_svy )
+
+      cat( paste0( 'WPP 2019 - Mean Age at Childbearing for ',
+                   get_location_name( location_code ),
+                   '\nReference year ',
+                   round( year_svy ),
+                   ': ',
+                   mac,
+                   '\n' ) )
+    }
 
     if( length( mac ) == 1 ){
       mac <- rep( mac, length( ages_w ) )
@@ -69,7 +95,35 @@ ceb_eval <-
               bty = 'n' )
     }
 
-    return( cbind(year_ceb,mean_ceb) )
+    return( cbind( year_ceb, mean_ceb ) )
   }
 
 
+#' Function to retrieve mean age of childbearing from Wpp 2019 data
+#'
+#' @param country_code list of country codes to retrieve fertility pattern data from
+#' @param year period of reference to retrieve fertility pattern data
+#'
+#' @return mean age at childbearing for country and year selected
+#'
+#' @keywords internal
+#'
+#'
+fetch_mac_Wpp2019 <- function( country_code = NULL, year ){
+
+  require( wpp2019 )
+  data('percentASFR')
+  year_interv <- findInterval( x = year, vec = seq( 1950, 2020, 5 ) )
+
+  year_sup <- seq( 1950, 2020, 5 )[ year_interv + 1 ]
+  year_inf <- seq( 1950, 2020, 5 )[ year_interv ]
+
+  # standardized fertility distribution of selected country
+  asfr_std <- c( percentASFR[ percentASFR$country_code %in% country_code,
+                              c( paste0( year_inf, '-', year_sup) ) ] / ( 5 * 100 ) )
+  ages <- seq( 15, 45, 5 )
+
+  mac <- round( crossprod( asfr_std, ages )/ sum( asfr_std ), 3 )
+
+  return( mac )
+}
