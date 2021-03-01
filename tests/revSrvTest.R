05c1808b5c52a066df5b8bec1db721d71b797180
josehcms <zecosta.monteiro@gmail.com>
2021-03-01 15:01:06 -0300

    comment test files

--- tests/revSrvTest.R
+++ tests/revSrvTest.R
@@ -1,75 +1,75 @@
-library(wpp2019)
-library(dplyr)
-library(fertestr)
-
-pop_c <-  c( 281260, 261320, 268410, 286810, 278990, 293760, 293490, 302060, 315970, 267190, 326980, 280260, 354120, 356920, 354830 )
-pop_w <- c(  815930, 780320, 697160, 626430, 361650, 435880, 393760, 352520, 294280, 230200, 160590, NA )
-lx_c <- c( 1.0000, 0.9320, 0.9275, 0.9228, 0.9165, 0.9125, 0.9110, 0.9094, 0.9079, 0.9063, 0.9048, 0.9032, 0.9017, 0.9001, 0.8986, 0.8970 )
-lx_w <- c( 0.91381, 0.90989, 0.90492, 0.89798, 0.88893, 0.87596, 0.86029, 0.84188, 0.81791, 0.78472, 0.73735, 0.67316 )
-q0_5 <-  c( 0.0683, 0.1008, 0.1189)
-q15_45 <- c( 0.1946, 0.2290, 0.2674)
-asfr <- c( 0.0000, 0.0418,0.1535, 0.1482, 0.1118, 0.0708, 0.0301, 0.0032 )
-asfr_std <- asfr/(5 * sum(asfr) )
-asfr_15prior <- c( 0.0000, 0.0533, 0.1974, 0.2144, 0.1836, 0.1332, 0.0676, 0.0134 )
-asfr_std_15prior <- asfr_15prior/(5 * sum(asfr_15prior) )
-
-FertRevSurv(ages1_c = 0:14,
-            popx1_c = pop_c,
-            ages5_w = seq(10,65,5),
-            popx5_w = pop_w,
-            lx1_c = lx_c,
-            lx5_w = lx_w,
-            asfr = asfr_std,
-            asfr_15prior = asfr_std_15prior,
-            q0_5 = q0_5,
-            q15_45f = q15_45,
-            date_ref = 2008)
-
-?FertRevSurv
-
-dic_year <-
-  c( '1965-1970' = '1967.5',
-    '1970-1975' = '1972.5',
-    '1975-1980' = '1977.5',
-    '1980-1985' = '1982.5',
-    '1985-1990' = '1987.5',
-    '1990-1995' = '1992.5',
-    '1995-2000' = '1997.5',
-    '2000-2005' = '2002.5',
-    '2005-2010' = '2007.5',
-    '2010-2015' = '2012.5',
-    '2015-2020' = '2017.5' )
-
-
-mxF <- load_named_data('mxF', "wpp2019")
-mxM <- load_named_data('mxM', "wpp2019")
-
-# error analysis
-listSample <- mxF[ mxF$country_code > 900, ]$country_code %>% unique
-
-sel_list = NULL
-# dados de mortalidade com repeticoes
-for( i in mxF$country_code %>% unique ){
-  lenF = length(mxF[ mxF$country_code == i, ]$age)
-  lenM = length(mxM[ mxM$country_code == i, ]$age)
-
-  if(lenM == 22){
-    sel_list = c(sel_list,i)
-  }
-  if(lenM != 22){
-    cat( paste0('Duplicates in mxM: ', i ,' (',mxM[ mxM$country_code == i,]$name %>% unique,')\n') )
-    cat( paste0( 'age vector:','\n' ) )
-    cat( mxM[ mxM$country_code == i,]$age )
-    cat( paste0( '\n' ) )
-  }
-
-  if(lenF != 22){
-    cat( paste0('Duplicates in mxF: ', i ,' (',mxF[ mxF$country_code == i,]$name %>% unique,')\n') )
-    cat( paste0( 'age vector:','\n' ) )
-    cat( mxF[ mxF$country_code == i,]$age )
-    cat( paste0( '\n' ) )
-  }
-}
+# # library(wpp2019)
+# # library(dplyr)
+# # library(fertestr)
+#
+# pop_c <-  c( 281260, 261320, 268410, 286810, 278990, 293760, 293490, 302060, 315970, 267190, 326980, 280260, 354120, 356920, 354830 )
+# pop_w <- c(  815930, 780320, 697160, 626430, 361650, 435880, 393760, 352520, 294280, 230200, 160590, NA )
+# lx_c <- c( 1.0000, 0.9320, 0.9275, 0.9228, 0.9165, 0.9125, 0.9110, 0.9094, 0.9079, 0.9063, 0.9048, 0.9032, 0.9017, 0.9001, 0.8986, 0.8970 )
+# lx_w <- c( 0.91381, 0.90989, 0.90492, 0.89798, 0.88893, 0.87596, 0.86029, 0.84188, 0.81791, 0.78472, 0.73735, 0.67316 )
+# q0_5 <-  c( 0.0683, 0.1008, 0.1189)
+# q15_45 <- c( 0.1946, 0.2290, 0.2674)
+# asfr <- c( 0.0000, 0.0418,0.1535, 0.1482, 0.1118, 0.0708, 0.0301, 0.0032 )
+# asfr_std <- asfr/(5 * sum(asfr) )
+# asfr_15prior <- c( 0.0000, 0.0533, 0.1974, 0.2144, 0.1836, 0.1332, 0.0676, 0.0134 )
+# asfr_std_15prior <- asfr_15prior/(5 * sum(asfr_15prior) )
+#
+# FertRevSurv(ages1_c = 0:14,
+#             popx1_c = pop_c,
+#             ages5_w = seq(10,65,5),
+#             popx5_w = pop_w,
+#             lx1_c = lx_c,
+#             lx5_w = lx_w,
+#             asfr = asfr_std,
+#             asfr_15prior = asfr_std_15prior,
+#             q0_5 = q0_5,
+#             q15_45f = q15_45,
+#             date_ref = 2008)
+#
+# ?FertRevSurv
+#
+# dic_year <-
+#   c( '1965-1970' = '1967.5',
+#     '1970-1975' = '1972.5',
+#     '1975-1980' = '1977.5',
+#     '1980-1985' = '1982.5',
+#     '1985-1990' = '1987.5',
+#     '1990-1995' = '1992.5',
+#     '1995-2000' = '1997.5',
+#     '2000-2005' = '2002.5',
+#     '2005-2010' = '2007.5',
+#     '2010-2015' = '2012.5',
+#     '2015-2020' = '2017.5' )
+#
+#
+# mxF <- load_named_data('mxF', "wpp2019")
+# mxM <- load_named_data('mxM', "wpp2019")
+#
+# # error analysis
+# listSample <- mxF[ mxF$country_code > 900, ]$country_code %>% unique
+#
+# sel_list = NULL
+# # dados de mortalidade com repeticoes
+# for( i in mxF$country_code %>% unique ){
+#   lenF = length(mxF[ mxF$country_code == i, ]$age)
+#   lenM = length(mxM[ mxM$country_code == i, ]$age)
+#
+#   if(lenM == 22){
+#     sel_list = c(sel_list,i)
+#   }
+#   if(lenM != 22){
+#     cat( paste0('Duplicates in mxM: ', i ,' (',mxM[ mxM$country_code == i,]$name %>% unique,')\n') )
+#     cat( paste0( 'age vector:','\n' ) )
+#     cat( mxM[ mxM$country_code == i,]$age )
+#     cat( paste0( '\n' ) )
+#   }
+#
+#   if(lenF != 22){
+#     cat( paste0('Duplicates in mxF: ', i ,' (',mxF[ mxF$country_code == i,]$name %>% unique,')\n') )
+#     cat( paste0( 'age vector:','\n' ) )
+#     cat( mxF[ mxF$country_code == i,]$age )
+#     cat( paste0( '\n' ) )
+#   }
+# }
 
 
 ## dat <-
